c6b18b7b443bf9ce7112090232fed6d37d4dc354
hkimjp <hkim@hkim.jp>
2025-11-24 14:38:18 +0900

    confirmed SSE working

--- src/hkimjp/sse_example/client.clj
+++ src/hkimjp/sse_example/client.clj
@@ -7,25 +7,25 @@
 (defn client [_request]
   (page
    [:div.m-4
-    [:div.py-2
+    [:div.py-2.flex.gap-x-4
      [:button.bg-sky-300.hover:bg-blue-600.active:bg-red-400.text-white.px-2
       {:hx-get    "/hx/now"
        :hx-target "#now"
        :hx-swap   "innerHTML"}
-      "What time?"]]
-    [:div [:span#now.border-1 "..."]]
+      "What time?"]
+     [:div [:span#now.border-1 "..."]]]
     [:div.text-2xl.font-bold.py-2 "SSE example, using htmx-ext-sse"]
-    [:form
+    [:form.flex.gap-x-4
      (h/raw (anti-forgery-field))
      [:input.border-1 {:name "bc"}]
      [:button.bg-sky-400.rounded.hover:bg-sky-600.text-white
       {:hx-post "/broadcast" :hx-swap "none"}
       "broadcast"]]
-    [:div
-     "broadcast ->" [:span
-                     {:hx-ext      "sse"
-                      :sse-connect "/event"
-                      :sse-swap    "message"}
-                     "Contents of this box will be updated in real time
-    with every SSE message received from the chatroom."]]]))
+    [:div.flex.gap-x-4
+     [:div "broadcast ->"]
+     [:div {:hx-ext      "sse"
+            :sse-connect "/event"
+            :sse-swap    "message"}
+      "Contents of this box will be updated in real time
+      with every SSE message received from the chatroom."]]]))
 
